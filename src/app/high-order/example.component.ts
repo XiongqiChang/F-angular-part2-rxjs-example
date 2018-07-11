2f676e298a88d8d64429c9ca1ca0b089b08ec4db
ljcheng <ljcheng@thoughtworks.com>
2018-07-11 16:57:56 +0800

    refactor high order examples

--- src/app/high-order/example.component.ts
+++ src/app/high-order/example.component.ts
@@ -8,10 +8,29 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HighOrderExampleComponent {
-  highOrder$: Observable<Observable<any>>;
+  highOrder$: Observable<Observable<number>>;
+  mergeAll$: Observable<number>;
+  concatAll$: Observable<number>;
+  switch$: Observable<number>;
+  tabs: any[];
 
   constructor() {
-    this.highOrder$ = Observable.timer(1000, 2000).take(3)
-      .map(n => Observable.timer(0, 1500).take(3).map(m => (n + 1) * 10 + m));
+    this.highOrder$ = Observable.concat(
+      Observable.never().takeUntil(Observable.timer(1000)),
+      Observable.of(Observable.timer(0, 2500).take(3).map(i => 10 + i)),
+      Observable.never().takeUntil(Observable.timer(3000)),
+      Observable.of(Observable.timer(0, 1500).take(5).map(i => 20 + i)),
+      Observable.never().takeUntil(Observable.timer(2500)),
+      Observable.of(Observable.timer(0, 1000).take(3).map(i => 30 + i)),
+    );
+    this.mergeAll$ = this.highOrder$.mergeAll();
+    this.concatAll$ = this.highOrder$.concatAll();
+    this.switch$ = this.highOrder$.switch();
+    this.tabs = [
+      { heading: 'None' },
+      { heading: 'Merge', observable: this.mergeAll$ },
+      { heading: 'Concat', observable: this.concatAll$ },
+      { heading: 'Switch', observable: this.switch$ },
+    ]
   }
 }
