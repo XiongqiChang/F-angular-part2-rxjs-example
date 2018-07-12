172b252a25f295b80b36f0883302025d81c3914b
ljcheng <ljcheng@thoughtworks.com>
2018-07-12 11:45:27 +0800

    remove useless code

--- src/app/_common/components/monitor-control/monitor-control.component.ts
+++ src/app/_common/components/monitor-control/monitor-control.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 import { TimerService } from '../../services/timer.service';
 
 @Component({
@@ -10,9 +9,7 @@ import { TimerService } from '../../services/timer.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MonitorControlComponent implements OnInit {
-  @Input() subject: Subject<any>;
   timerText$: Observable<string>;
-  count: number = 0;
 
   constructor(private timerService: TimerService) {
   }
@@ -23,11 +20,5 @@ export class MonitorControlComponent implements OnInit {
 
   toggleTimer() {
     this.timerService.toggle();
-  }
-
-  next() {
-    if (this.subject) {
-      this.subject.next(this.count += 1);
-    }
   }
 }
