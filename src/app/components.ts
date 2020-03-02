86cafab74666c425565812538017731e63f68e51
ljcheng <ljcheng@thoughtworks.com>
2020-03-02 14:26:17 +0800

    no render inactive tabs

--- src/app/components.ts
+++ src/app/components.ts
@@ -1,5 +1,5 @@
 import { NoContentComponent } from './no-content/no-content';
-import { RxJSExampleComponent } from './rxjs-example/rxjs-example.component';
+// import { RxJSExampleComponent } from './rxjs-example/rxjs-example.component';
 import { MonitorControlComponent } from './_common/components/monitor-control/monitor-control.component';
 import { ObservableMonitorComponent } from './_common/components/observable-monitor/observable-monitor.component';
 import { ObservableExampleComponent } from './observable-example/observable-example.component';
